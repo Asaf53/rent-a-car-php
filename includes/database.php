a9c0fb550258604cb4a5c69ea43ae530ce4e6fa2
Asaf Rushiti <asafrushiti3@gmail.com>
2025-02-26 01:09:36 +0100

    Update database.php

--- includes/database.php
+++ includes/database.php
@@ -3,5 +3,13 @@ $host = 'sql.freedb.tech';
 $db = 'freedb_rent-a-car';
 $dsn = "mysql:host=$host;dbname=$db;";
 $username = 'freedb_Asafrushiti';
-$password = 'fQ@DpWv9ZG7aF#2';
-$pdo = new PDO($dsn, $username, $password);
+$password = 'fQ@DpWv9ZG7aF%232'; // %23 is the URL encoding for the '#' character
+try {
+    $pdo = new PDO($dsn, $username, $password);
+    // Set the PDO error mode to exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connection successful!";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
