2428f27f17b1517925ea94f31d4246a2189cae6d
Asaf Rushiti <asafrushiti3@gmail.com>
2025-02-26 01:12:36 +0100

    Update database.php

--- includes/database.php
+++ includes/database.php
@@ -3,7 +3,7 @@ $host = 'sql.freedb.tech';
 $db = 'freedb_rent-a-car';
 $dsn = "mysql:host=$host;dbname=$db;";
 $username = 'freedb_Asafrushiti';
-$password = 'fQ@DpWv9ZG7aF%232'; // %23 is the URL encoding for the '#' character
+$password = 'fQ@DpWv9ZG7aF#2'; // %23 is the URL encoding for the '#' character
 try {
     $pdo = new PDO($dsn, $username, $password);
     // Set the PDO error mode to exception
