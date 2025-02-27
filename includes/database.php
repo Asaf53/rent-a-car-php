66abc85fdfa03fe41b5106704f8c2a799f50267e
Asaf53 <asafrushiti3@gmail.com>
2025-02-27 01:19:10 +0100

    new

--- includes/database.php
+++ includes/database.php
@@ -1,9 +1,9 @@
 <?php
-$host = getenv('DB_HOST');
-$port = getenv('DB_PORT');
-$db = getenv('DB_NAME');
-$username = getenv('DB_USER');
-$password = getenv('DB_PASS');
+$host = getenv('DB_HOST') ?: 'sql.freedb.tech'; // Do NOT use 'localhost'
+$port = getenv('DB_PORT') ?: '3306';
+$db = getenv('DB_NAME') ?: 'freedb_rent-a-car';
+$username = getenv('DB_USER') ?: 'freedb_Asafrushiti';
+$password = getenv('DB_PASS') ?: 'fQ@DpWv9ZG7aF#2';
 
 $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4";
 
