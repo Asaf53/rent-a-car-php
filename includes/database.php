9ebe6fffd3c42e439d554f00ac4a303978ed5156
Asaf Rushiti <asafrushiti3@gmail.com>
2025-02-27 01:01:54 +0100

    Update database.php

--- includes/database.php
+++ includes/database.php
@@ -4,7 +4,7 @@ $port = 3306; // Default MySQL port
 $db = 'freedb_rent-a-car';
 $dsn = "mysql:host=$host;port=$port;dbname=$db;";
 $username = 'freedb_Asafrushiti';
-$password = 'fQ@DpWv9ZG7aF%232'; // URL-encoded password
+$password = 'fQ@DpWv9ZG7aF#2'; // URL-encoded password
 
 try {
     $pdo = new PDO($dsn, $username, $password);
